3b371db973add6000f6e8f9f2dc2790529aa993a
Andrea Giacobino <no.andrea@gmail.com>
2018-08-17 14:26:23 +0200

    Fix decode/encode/hash functions and add tests

--- aeternity/crypto.go
+++ aeternity/crypto.go
@@ -34,32 +34,41 @@ func Load(priv string) (kp *KeyPair, err error) {
 	return
 }
 
+func h(data []byte) []byte {
+	d := sha256.New()
+	d.Write(data)
+	return d.Sum(nil)
+}
+
+// encode encode a byte array into base58 with chacksum
 func encode(in []byte) string {
-	h := func(data []byte) []byte {
-		d := sha256.New()
-		d.Write(data)
-		return d.Sum(nil)
-	}
 	c := h(h(in))
 	return base58.Encode(append(in, c[0:4]...))
 }
 
-func encodeP(prefix string, data []byte) string {
+// encodeP encode a byte array into base58 with chacksum and a prefix
+func encodeP(prefix HashPrefix, data []byte) string {
 	return fmt.Sprint(prefix, encode(data))
 }
 
+// decode decode a string encoded with base58 + checksum to a byte array
 func decode(in string) (out []byte, err error) {
-	strings.HasPrefix("aa", "")
-	if string(in[2]) == "$" {
+	if len(in) >= 3 && string(in[2]) == "$" {
 		in = in[3:]
 	}
-	out = base58.Decode(in)
-	c := encode(out[len(out)-4:])
-	if in != c {
-		err = fmt.Errorf("Invalid checksum")
+	raw := base58.Decode(in)
+	if len(raw) < 5 {
+		err = fmt.Errorf("Invalid input, %s cannot be decoded", in)
+		return
+	}
+	out = raw[:len(raw)-4]
+	if chk := encode(out); in != chk {
+		err = fmt.Errorf("Invalid checksum, expected %s got %s", chk, in)
 	}
 	return
 }
+
+// hash calculate the blacke2b 32bit hash of the input byte array
 func hash(in []byte) (out []byte, err error) {
 	h, err := blake2b.New(32, nil)
 	if err != nil {
--- aeternity/crypto_test.go
+++ aeternity/crypto_test.go
@@ -35,25 +35,67 @@ func TestLoad(t *testing.T) {
 }
 
 func TestKeyPair_Sign(t *testing.T) {
+
+	priv := "4d881dd1917036cc231f9881a0db978c8899dd76a817252418606b02bf6ab9d22378f892b7cc82c2d2739e994ec9953aa36461f1eb5a4a49a5b0de17b3d23ae8"
+	pub := "ak$Gd6iMVsoonGuTF8LeswwDDN2NF5wYHAoTRtzwdEcfS32LWoxm"
+	kp, _ := Load(priv)
+
+	if kp.Address != pub {
+		t.Errorf("Load() expected = %v, got %v", pub, kp.Address)
+	}
+
+	txBinaryAsArray := []byte{248, 76, 12, 1, 160, 35, 120, 248, 146, 183, 204, 130, 194, 210, 115, 158, 153, 78, 201, 149, 58, 163, 100, 97, 241, 235, 90, 74, 73, 165, 176, 222, 23, 179, 210, 58, 232, 160, 63, 40, 35, 12, 40, 65, 38, 215, 218, 236, 136, 133, 42, 120, 160, 179, 18, 191, 241, 162, 198, 203, 209, 173, 89, 136, 202, 211, 158, 59, 12, 122, 1, 1, 1, 132, 84, 101, 115, 116}
+	signatureAsArray := []byte{95, 146, 31, 37, 95, 194, 36, 76, 58, 49, 167, 156, 127, 131, 142, 248, 25, 121, 139, 109, 59, 243, 203, 205, 16, 172, 115, 143, 254, 236, 33, 4, 43, 46, 16, 190, 46, 46, 140, 166, 76, 39, 249, 54, 38, 27, 93, 159, 58, 148, 67, 198, 81, 206, 106, 237, 91, 131, 27, 14, 143, 178, 130, 2}
+
 	type args struct {
 		message []byte
 	}
 	tests := []struct {
 		name          string
-		fields        KeyPair
+		keyPair       *KeyPair
 		args          args
 		wantSignature []byte
 	}{
-		// TODO: Add test cases.
+		{"ok", kp, args{txBinaryAsArray}, signatureAsArray},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			k := &KeyPair{
-				SigningKey: tt.fields.SigningKey,
-				Address:    tt.fields.Address,
-			}
-			if gotSignature := k.Sign(tt.args.message); !reflect.DeepEqual(gotSignature, tt.wantSignature) {
+			if gotSignature := tt.keyPair.Sign(tt.args.message); !reflect.DeepEqual(gotSignature, tt.wantSignature) {
 				t.Errorf("KeyPair.Sign() = %v, want %v", gotSignature, tt.wantSignature)
+			}
+		})
+	}
+}
+
+func Test_decode(t *testing.T) {
+	type args struct {
+		in string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut []byte
+		wantErr bool
+	}{
+		{"invalid", args{"c"}, []byte{}, true},
+		{"invalid", args{"cd"}, []byte{}, true},
+		{"invalid", args{"ak$"}, []byte{}, true},
+		{"invalid", args{"ak$aasda"}, []byte{}, true},
+		{"invalid", args{"000000"}, []byte{}, true},
+		{"invalid", args{"0"}, []byte{}, true},
+		{"invalid", args{"ak$0"}, []byte{}, true},
+		{"valid", args{"ak$Gd6iMVsoonGuTF8LeswwDDN2NF5wYHAoTRtzwdEcfS32LWoxm"}, []byte{35, 120, 248, 146, 183, 204, 130, 194, 210, 115, 158, 153, 78, 201, 149, 58, 163, 100, 97, 241, 235, 90, 74, 73, 165, 176, 222, 23, 179, 210, 58, 232}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOut, err := decode(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("decode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr && !reflect.DeepEqual(gotOut, tt.wantOut) {
+				t.Errorf("decode() = %v, want %v", gotOut, tt.wantOut)
 			}
 		})
 	}
