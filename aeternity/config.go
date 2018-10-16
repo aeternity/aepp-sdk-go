f11e1aeaed01bbd1372ed8c0a899fd2864abe8b8
Andrea Giacobino <no.andrea@gmail.com>
2018-10-16 15:15:07 +0200

    Update codebase compatible with the generated client changes

--- aeternity/config.go
+++ aeternity/config.go
@@ -1,103 +1,103 @@
 package aeternity
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "path/filepath"
+  "strings"
+  "time"
 
-	utils "github.com/aeternity/aepp-sdk-go/utils"
+  utils "github.com/aeternity/aepp-sdk-go/utils"
 
-	yaml "gopkg.in/yaml.v2"
+  yaml "gopkg.in/yaml.v2"
 )
 
 const (
-	// ConfigFilename default configuration file name
-	ConfigFilename = "config"
+  // ConfigFilename default configuration file name
+  ConfigFilename = "config"
 )
 
 // EpochConfig configuration for the epoch node
 type EpochConfig struct {
-	URL string `yaml:"url" mapstructure:"url"`
+  URL string `yaml:"url" mapstructure:"url"`
 }
 
 // AensConfig configurations for Aens
 type AensConfig struct {
-	TTL         int64 `yaml:"ttl" mapstructure:"ttl"`
-	ClientTTL   int64 `yaml:"client_ttl" mapstructure:"client_ttl"`
-	PreClaimFee int64 `yaml:"preclaim_fee" mapstructure:"preclaim_fee"`
-	ClaimFee    int64 `yaml:"claim_fee" mapstructure:"claim_fee"`
-	UpdateFee   int64 `yaml:"update_fee" mapstructure:"update_fee"`
+  TTL         uint64 `yaml:"ttl" mapstructure:"ttl"`
+  ClientTTL   uint64 `yaml:"client_ttl" mapstructure:"client_ttl"`
+  PreClaimFee int64  `yaml:"preclaim_fee" mapstructure:"preclaim_fee"`
+  ClaimFee    int64  `yaml:"claim_fee" mapstructure:"claim_fee"`
+  UpdateFee   int64  `yaml:"update_fee" mapstructure:"update_fee"`
 }
 
 // ContractConfig configurations for contracts
 type ContractConfig struct {
-	Gas       int64 `yaml:"gas" mapstructure:"gas"`
-	GasPrice  int64 `yaml:"gas_price" mapstructure:"gas_price"`
-	Deposit   int64 `yaml:"deposit" mapstructure:"deposit"`
-	VMVersion int64 `yaml:"vm_version" mapstructure:"vm_version"`
+  Gas       int64 `yaml:"gas" mapstructure:"gas"`
+  GasPrice  int64 `yaml:"gas_price" mapstructure:"gas_price"`
+  Deposit   int64 `yaml:"deposit" mapstructure:"deposit"`
+  VMVersion int64 `yaml:"vm_version" mapstructure:"vm_version"`
 }
 
 // ClientConfig client paramters configuration
 type ClientConfig struct {
-	TxTTL      int64          `yaml:"tx_ttl" mapstructure:"tx_ttl"`
-	Fee        int64          `yaml:"fee" mapstructure:"fee"`
-	DefaultKey string         `yaml:"default_key_name" mapstructure:"default_key_name"`
-	Names      AensConfig     `yaml:"names" mapstructure:"names"`
-	Contracts  ContractConfig `yaml:"contracts" mapstructure:"contracts"`
+  TxTTL      uint64         `yaml:"tx_ttl" mapstructure:"tx_ttl"`
+  Fee        int64          `yaml:"fee" mapstructure:"fee"`
+  DefaultKey string         `yaml:"default_key_name" mapstructure:"default_key_name"`
+  Names      AensConfig     `yaml:"names" mapstructure:"names"`
+  Contracts  ContractConfig `yaml:"contracts" mapstructure:"contracts"`
 }
 
 // TuningConfig fine tuning of parameters of the client
 type TuningConfig struct {
-	ChainPollInteval int64 `yaml:"chain_poll_interval" mapstructure:"chain_poll_interval"`
-	ChainTimeout     int64 `yaml:"chain_timeout" mapstructure:"chain_timeout"`
-	OutputFormatJSON bool  `yaml:"-" mapstructure:"-"`
+  ChainPollInteval int64 `yaml:"chain_poll_interval" mapstructure:"chain_poll_interval"`
+  ChainTimeout     int64 `yaml:"chain_timeout" mapstructure:"chain_timeout"`
+  OutputFormatJSON bool  `yaml:"-" mapstructure:"-"`
 }
 
 // ProfileConfig a configuration profile
 type ProfileConfig struct {
-	Name   string       `yaml:"name" mapstructure:"name"`
-	Epoch  EpochConfig  `yaml:"epoch" mapstructure:"epoch"`
-	Client ClientConfig `yaml:"client" mapstructure:"client"`
-	Tuning TuningConfig `yaml:"tuning" mapstructure:"tuning"`
+  Name   string       `yaml:"name" mapstructure:"name"`
+  Epoch  EpochConfig  `yaml:"epoch" mapstructure:"epoch"`
+  Client ClientConfig `yaml:"client" mapstructure:"client"`
+  Tuning TuningConfig `yaml:"tuning" mapstructure:"tuning"`
 }
 
 // ConfigSchema define the configuration object
 type ConfigSchema struct {
-	ActiveProfile string           `yaml:"active_profile" mapstructure:"active_profile"`
-	Profiles      []*ProfileConfig `yaml:"profiles" mapstructure:"profiles"`
-	P             *ProfileConfig   `yaml:"-" mapstructure:"-"` // holds the active profile
-	ConfigPath    string           `yaml:"-" mapstructure:"-"` // the path of the configuration file
-	KeysFolder    string           `yaml:"-" mapstructure:"-"`
+  ActiveProfile string           `yaml:"active_profile" mapstructure:"active_profile"`
+  Profiles      []*ProfileConfig `yaml:"profiles" mapstructure:"profiles"`
+  P             *ProfileConfig   `yaml:"-" mapstructure:"-"` // holds the active profile
+  ConfigPath    string           `yaml:"-" mapstructure:"-"` // the path of the configuration file
+  KeysFolder    string           `yaml:"-" mapstructure:"-"`
 }
 
 //Defaults generate configuration defaults
 func (c *ProfileConfig) Defaults() *ProfileConfig {
-	c.Client = ClientConfig{
-		Contracts: ContractConfig{},
-		Names:     AensConfig{},
-	}
-	// for server
-	utils.DefaultIfEmptyStr(&c.Epoch.URL, "https://sdk-edgenet.aepps.com")
-	// for client
-	utils.DefaultIfEmptyStr(&c.Client.DefaultKey, "wallet.key")
-	utils.DefaultIfEmptyInt64(&c.Client.TxTTL, 500)
-	utils.DefaultIfEmptyInt64(&c.Client.Fee, 1)
-	// for aens
-	utils.DefaultIfEmptyInt64(&c.Client.Names.TTL, 500)
-	utils.DefaultIfEmptyInt64(&c.Client.Names.ClientTTL, 500)
-	utils.DefaultIfEmptyInt64(&c.Client.Names.PreClaimFee, 1)
-	utils.DefaultIfEmptyInt64(&c.Client.Names.ClaimFee, 1)
-	utils.DefaultIfEmptyInt64(&c.Client.Names.UpdateFee, 1)
-	// for contracts
-	utils.DefaultIfEmptyInt64(&c.Client.Contracts.Gas, 40000000)
-	utils.DefaultIfEmptyInt64(&c.Client.Contracts.GasPrice, 1)
-	// for tuning
-	utils.DefaultIfEmptyInt64(&c.Tuning.ChainPollInteval, 1000)
-	utils.DefaultIfEmptyInt64(&c.Tuning.ChainTimeout, 5000)
-	return c
+  c.Client = ClientConfig{
+    Contracts: ContractConfig{},
+    Names:     AensConfig{},
+  }
+  // for server
+  utils.DefaultIfEmptyStr(&c.Epoch.URL, "https://sdk-edgenet.aepps.com")
+  // for client
+  utils.DefaultIfEmptyStr(&c.Client.DefaultKey, "wallet.key")
+  utils.DefaultIfEmptyUint64(&c.Client.TxTTL, 500)
+  utils.DefaultIfEmptyInt64(&c.Client.Fee, 1)
+  // for aens
+  utils.DefaultIfEmptyUint64(&c.Client.Names.TTL, 500)
+  utils.DefaultIfEmptyUint64(&c.Client.Names.ClientTTL, 500)
+  utils.DefaultIfEmptyInt64(&c.Client.Names.PreClaimFee, 1)
+  utils.DefaultIfEmptyInt64(&c.Client.Names.ClaimFee, 1)
+  utils.DefaultIfEmptyInt64(&c.Client.Names.UpdateFee, 1)
+  // for contracts
+  utils.DefaultIfEmptyInt64(&c.Client.Contracts.Gas, 40000000)
+  utils.DefaultIfEmptyInt64(&c.Client.Contracts.GasPrice, 1)
+  // for tuning
+  utils.DefaultIfEmptyInt64(&c.Tuning.ChainPollInteval, 1000)
+  utils.DefaultIfEmptyInt64(&c.Tuning.ChainTimeout, 5000)
+  return c
 }
 
 //Validate configuration
@@ -107,26 +107,26 @@ func (c *ProfileConfig) Validate() {
 
 //Validate configuration
 func (c *ConfigSchema) Validate() {
-	valid := false
-	for _, p := range c.Profiles {
-		if p.Name == c.ActiveProfile {
-			valid = true
-		}
-	}
+  valid := false
+  for _, p := range c.Profiles {
+    if p.Name == c.ActiveProfile {
+      valid = true
+    }
+  }
 
-	if !valid {
-		fmt.Println("Invalid configuration")
-		os.Exit(1)
-	}
+  if !valid {
+    fmt.Println("Invalid configuration")
+    os.Exit(1)
+  }
 }
 
 //Defaults configuration
 func (c *ConfigSchema) Defaults() *ConfigSchema {
-	for _, p := range c.Profiles {
-		p.Defaults()
-	}
-	c.ActivateProfile(c.ActiveProfile)
-	return c
+  for _, p := range c.Profiles {
+    p.Defaults()
+  }
+  c.ActivateProfile(c.ActiveProfile)
+  return c
 }
 
 // Config sytem configuration
@@ -134,63 +134,63 @@ var Config ConfigSchema
 
 // GenerateDefaultConfig generate a default configuration
 func GenerateDefaultConfig(outFile, version string) {
-	Config = ConfigSchema{
-		KeysFolder: filepath.Join(filepath.Dir(outFile), "keys"),
-		ConfigPath: outFile,
-	}
-	Config.NewProfile("default")
-	Config.ActivateProfile("default")
+  Config = ConfigSchema{
+    KeysFolder: filepath.Join(filepath.Dir(outFile), "keys"),
+    ConfigPath: outFile,
+  }
+  Config.NewProfile("default")
+  Config.ActivateProfile("default")
 }
 
 // Save save the configuration to disk
 func (c *ConfigSchema) Save() {
-	b, _ := yaml.Marshal(c)
-	data := strings.Join([]string{
-		"#",
-		"#\n# Configuration for aepp-sdk-go \n#\n",
-		fmt.Sprintf("#\n# update on %s \n#\n", time.Now().Format(time.RFC3339)),
-		fmt.Sprintf("%s", b),
-		"#\n# Config end\n#",
-	}, "\n")
-	if err := os.MkdirAll(filepath.Dir(c.ConfigPath), os.ModePerm); err != nil {
-		fmt.Println("Cannot create config file path", err)
-		os.Exit(1)
-	}
-	err := ioutil.WriteFile(c.ConfigPath, []byte(data), 0600)
-	if err != nil {
-		fmt.Println("Cannot create config file ", err)
-		os.Exit(1)
-	}
-	fmt.Println("config file written to", c.ConfigPath)
+  b, _ := yaml.Marshal(c)
+  data := strings.Join([]string{
+    "#",
+    "#\n# Configuration for aepp-sdk-go \n#\n",
+    fmt.Sprintf("#\n# update on %s \n#\n", time.Now().Format(time.RFC3339)),
+    fmt.Sprintf("%s", b),
+    "#\n# Config end\n#",
+  }, "\n")
+  if err := os.MkdirAll(filepath.Dir(c.ConfigPath), os.ModePerm); err != nil {
+    fmt.Println("Cannot create config file path", err)
+    os.Exit(1)
+  }
+  err := ioutil.WriteFile(c.ConfigPath, []byte(data), 0600)
+  if err != nil {
+    fmt.Println("Cannot create config file ", err)
+    os.Exit(1)
+  }
+  fmt.Println("config file written to", c.ConfigPath)
 }
 
 // NewProfile create a new configuration profile
 func (c *ConfigSchema) NewProfile(name string) {
-	p := &ProfileConfig{
-		Name: name,
-	}
-	p.Defaults()
-	c.Profiles = append(c.Profiles, p)
+  p := &ProfileConfig{
+    Name: name,
+  }
+  p.Defaults()
+  c.Profiles = append(c.Profiles, p)
 }
 
 // ActivateProfile a profile, err if the profile doesnt exists
 func (c *ConfigSchema) ActivateProfile(name string) (err error) {
-	p, err := c.GetProfile(name)
-	if err != nil {
-		return
-	}
-	c.ActiveProfile = name
-	c.P = p
-	return
+  p, err := c.GetProfile(name)
+  if err != nil {
+    return
+  }
+  c.ActiveProfile = name
+  c.P = p
+  return
 }
 
 // GetProfile get a profile by name, err when doenst exists
 func (c *ConfigSchema) GetProfile(name string) (p *ProfileConfig, err error) {
-	for _, p = range c.Profiles {
-		if p.Name == name {
-			return
-		}
-	}
-	err = fmt.Errorf("Profile %s not found", name)
-	return
+  for _, p = range c.Profiles {
+    if p.Name == name {
+      return
+    }
+  }
+  err = fmt.Errorf("Profile %s not found", name)
+  return
 }
--- cmd/inspect.go
+++ cmd/inspect.go
@@ -15,95 +15,95 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
+  "fmt"
+  "os"
+  "regexp"
+  "strconv"
+  "strings"
 
-	"github.com/aeternity/aepp-sdk-go/aeternity"
+  "github.com/aeternity/aepp-sdk-go/aeternity"
 
-	"github.com/spf13/cobra"
+  "github.com/spf13/cobra"
 )
 
 // inspectCmd represents the inspect command
 var inspectCmd = &cobra.Command{
-	Use:   "inspect",
-	Short: "Inspect an object of the blockchain",
-	Long: `Inspect an object of the chain
+  Use:   "inspect",
+  Short: "Inspect an object of the blockchain",
+  Long: `Inspect an object of the chain
 
 Valid object to inspect are block hash, transaction hash, accounts`,
-	Run:  inspect,
-	Args: cobra.MinimumNArgs(1),
+  Run:  inspect,
+  Args: cobra.MinimumNArgs(1),
 }
 
 func init() {
-	RootCmd.AddCommand(inspectCmd)
+  RootCmd.AddCommand(inspectCmd)
 
-	// Here you will define your flags and configuration settings.
+  // Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// inspectCmd.PersistentFlags().String("foo", "", "A help for foo")
+  // Cobra supports Persistent Flags which will work for this command
+  // and all subcommands, e.g.:
+  // inspectCmd.PersistentFlags().String("foo", "", "A help for foo")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// inspectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  // Cobra supports local flags which will only run when this command
+  // is called directly, e.g.:
+  // inspectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 }
 
 func printResult(v interface{}, err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	aeternity.PrintObject(v)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  aeternity.PrintObject(v)
 }
 
 func inspect(cmd *cobra.Command, args []string) {
-	for _, object := range args {
-		// height
-		if matched, _ := regexp.MatchString(`^\d+$`, object); matched {
-			height, _ := strconv.ParseInt(object, 10, 64)
-			aeCli.PrintGenerationByHeight(height)
-			continue
-		}
-		// name
-		if strings.HasSuffix(object, ".aet") {
-			v, err := aeCli.APIGetNameEntryByName(object)
-			printResult(v, err)
-			continue
-		}
+  for _, object := range args {
+    // height
+    if matched, _ := regexp.MatchString(`^\d+$`, object); matched {
+      height, _ := strconv.ParseUint(object, 10, 64)
+      aeCli.PrintGenerationByHeight(height)
+      continue
+    }
+    // name
+    if strings.HasSuffix(object, ".aet") {
+      v, err := aeCli.APIGetNameEntryByName(object)
+      printResult(v, err)
+      continue
+    }
 
-		switch aeternity.GetHashPrefix(object) {
-		case aeternity.PrefixAccount:
-			// account balance
-			v, err := aeCli.APIGetAccount(object)
-			printResult(v, err)
+    switch aeternity.GetHashPrefix(object) {
+    case aeternity.PrefixAccount:
+      // account balance
+      v, err := aeCli.APIGetAccount(object)
+      printResult(v, err)
 
-		case aeternity.PrefixMicroBlockHash:
-			v, err := aeCli.APIGetMicroBlockHeaderByHash(object)
-			printResult(v, err)
-			v1, err := aeCli.APIGetMicroBlockTransactionsByHash(object)
-			printResult(v1, err)
+    case aeternity.PrefixMicroBlockHash:
+      v, err := aeCli.APIGetMicroBlockHeaderByHash(object)
+      printResult(v, err)
+      v1, err := aeCli.APIGetMicroBlockTransactionsByHash(object)
+      printResult(v1, err)
 
-		case aeternity.PrefixKeyBlockHash:
-			// block
-			v, err := aeCli.APIGetKeyBlockByHash(object)
-			printResult(v, err)
+    case aeternity.PrefixKeyBlockHash:
+      // block
+      v, err := aeCli.APIGetKeyBlockByHash(object)
+      printResult(v, err)
 
-		case aeternity.PrefixTxHash:
-			// transaction
-			v, err := aeCli.APIGetTransactionByHash(object)
-			printResult(v, err)
+    case aeternity.PrefixTxHash:
+      // transaction
+      v, err := aeCli.APIGetTransactionByHash(object)
+      printResult(v, err)
 
-		case aeternity.PrefixOracle:
-			// oracle
-			v, err := aeCli.APIGetOracleByPubkey(object)
-			printResult(v, err)
+    case aeternity.PrefixOracle:
+      // oracle
+      v, err := aeCli.APIGetOracleByPubkey(object)
+      printResult(v, err)
 
-		default:
-			fmt.Println("Object", object, "not yet supported")
-		}
-	}
+    default:
+      fmt.Println("Object", object, "not yet supported")
+    }
+  }
 }
