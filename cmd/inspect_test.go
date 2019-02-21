d52eb1e1a24dc996b18fc6cea31cdd14d4363069
Andrew Chiw <randomshinichi4869@gmail.com>
2019-02-21 17:38:28 +0100

    cmd: inspect unittest and error object

--- cmd/inspect_test.go
+++ cmd/inspect_test.go
@@ -9,5 +9,8 @@ import (
 func TestInspect(t *testing.T) {
 	setCLIConfig()
 	emptyCmd := cobra.Command{}
-	inspectFunc(&emptyCmd, []string{"ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi"})
+	err := inspectFunc(&emptyCmd, []string{"ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi"})
+	if err != nil {
+		t.Error(err)
+	}
 }
