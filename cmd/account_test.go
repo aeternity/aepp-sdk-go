5ba5fbaf6b7ea00282996b4cc89f7d5018ecbbb9
Andrew Chiw <randomshinichi4869@gmail.com>
2019-02-27 14:31:41 +0100

    cmd: account balance unittest for when an account was found, and not found. Need to rework terminal.go to unwrap model.Error messages properly

--- cmd/account_test.go
+++ cmd/account_test.go
@@ -64,7 +64,7 @@ func TestAccountSave(t *testing.T) {
 	}
 }
 
-func TestAccountBalance(t *testing.T) {
+func TestAccountBalanceNotFound(t *testing.T) {
 	setCLIConfig()
 	password = "password"
 	emptyCmd := cobra.Command{}
@@ -76,6 +76,28 @@ func TestAccountBalance(t *testing.T) {
 	os.Chdir(dir)
 
 	err = createFunc(&emptyCmd, []string{"test.json"})
+	if err != nil {
+		t.Error(err)
+	}
+	err = balanceFunc(&emptyCmd, []string{"test.json"})
+	if err == nil {
+		t.Error("Expected 'Account not found' error but got nil instead")
+	}
+}
+
+func TestAccountBalanceFound(t *testing.T) {
+	setCLIConfig()
+	password = "password"
+	privateKey := "e6a91d633c77cf5771329d3354b3bcef1bc5e032c43d70b6d35af923ce1eb74dcea7ade470c9f99d9d4e400880a86f1d49bb444b62f11a9ebb64bbcfeb73fef3"
+	emptyCmd := cobra.Command{}
+	dir, err := ioutil.TempDir("", "aecli")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.RemoveAll(dir)
+	os.Chdir(dir)
+
+	err = saveFunc(&emptyCmd, []string{"test.json", privateKey})
 	if err != nil {
 		t.Error(err)
 	}
