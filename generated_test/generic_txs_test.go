0059263ea6d26460aeb0295fe3d2ee178b8cfde3
Andrew Chiw <randomshinichi4869@gmail.com>
2019-04-18 17:56:29 +0200

    fix: unittest for GenericTx deserialization

---
+++ generated_test/generic_txs_test.go
@@ -0,0 +1,19 @@
+package generated_test
+
+import (
+	"testing"
+
+	"github.com/aeternity/aepp-sdk-go/generated/models"
+)
+
+// Unfortunately, one must modify the generated go-swagger code.
+
+// Ideally the test JSON snippet should include every possible transaction type, but it only tests SpendTx for now.
+func TestGenericTxsPolymorphicDeserialization(t *testing.T) {
+	genericTxsJSON := []byte("{\"transactions\":[{\"block_hash\":\"mh_qoxFYgZoG7NxocZqBk1Dx9DJZKboT9WCKGFWV26QHKhB3oPDp\",\"block_height\":8835,\"hash\":\"th_uRnWPL3iqiLB7MzsVQ3aAgHsFALd62cmcNkw7ea1n9sfXybcr\",\"signatures\":[\"sg_RBq5vRuRchZ26HCPnZk5Xorn3ooQtfZSxf4mEPCsnpV9UD9KuZvqEKHM3vosoVvCvxvF5CyfdDkzCHnYW8bs3Ai7EtMBY\"],\"tx\":{\"amount\":10,\"fee\":20000000000000,\"nonce\":54,\"payload\":\"Hello World\",\"recipient_id\":\"ak_Egp9yVdpxmvAfQ7vsXGvpnyfNq71msbdUpkMNYGTeTe8kPL3v\",\"sender_id\":\"ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi\",\"ttl\":9335,\"type\":\"SpendTx\",\"version\":1}}]}")
+	genericTxs := models.GenericTxs{}
+	err := genericTxs.UnmarshalBinary(genericTxsJSON)
+	if err != nil {
+		t.Error(err)
+	}
+}
