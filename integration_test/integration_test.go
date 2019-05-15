07cb5dfd88868104ad86ff37ec7f74b90047adf7
Andrew Chiw <randomshinichi4869@gmail.com>
2019-05-15 22:59:15 +0200

    feat: prototype Contract Integration Test

--- integration_test/integration_test.go
+++ integration_test/integration_test.go
@@ -360,3 +360,37 @@ func TestOracleWorkflow(t *testing.T) {
 	_ = waitForTransaction(aeClient, oracleRespondTxHash)
 
 }
+
+func TestContracts(t *testing.T) {
+	acc, err := aeternity.AccountFromHexString(senderPrivateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	aeternity.Config.Node.NetworkID = networkID
+	aeClient := aeternity.NewCli(nodeURL, false).WithAccount(acc)
+
+	ttl, nonce, _ := aeClient.GetTTLNonce(acc.Address, aeternity.Config.Client.TTL)
+	code := "cb_+QP1RgKgpVq1Ib2r2ug+UktHvfWSQ8P35HJQHM6qikqBu1DwgtT5Avv5ASqgaPJnYzj/UIg5q6R3Se/6i+h+8oTyB/s9mZhwHNU4h8WEbWFpbrjAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKD//////////////////////////////////////////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+QHLoLnJVvKLMUmp9Zh6pQXz2hsiCcxXOSNABiu2wb2fn5nqhGluaXS4YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP//////////////////////////////////////////7kBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEA//////////////////////////////////////////8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA///////////////////////////////////////////uMxiAABkYgAAhJGAgIBRf7nJVvKLMUmp9Zh6pQXz2hsiCcxXOSNABiu2wb2fn5nqFGIAAMBXUIBRf2jyZ2M4/1CIOaukd0nv+ovofvKE8gf7PZmYcBzVOIfFFGIAAK9XUGABGVEAW2AAGVlgIAGQgVJgIJADYAOBUpBZYABRWVJgAFJgAPNbYACAUmAA81tZWWAgAZCBUmAgkANgABlZYCABkIFSYCCQA2ADgVKBUpBWW2AgAVFRWVCAkVBQgJBQkFZbUFCCkVBQYgAAjFaFMi4xLjBJtQib"
+	callData := "cb_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACC5yVbyizFJqfWYeqUF89obIgnMVzkjQAYrtsG9n5+Z6gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAnHQYrA=="
+	fmt.Println("Contract Create")
+	tx := aeternity.NewContractCreateTx(acc.Address, nonce, code, aeternity.Config.Client.Contracts.VMVersion, aeternity.Config.Client.Contracts.ABIVersion, aeternity.Config.Client.Contracts.Deposit, aeternity.Config.Client.Contracts.Amount, *utils.NewBigIntFromUint64(1e6), aeternity.Config.Client.Contracts.GasPrice, *utils.NewBigIntFromUint64(1.2e17), ttl, callData)
+
+	ctID, _ := tx.ContractID()
+	txStr, _ := aeternity.BaseEncodeTx(&tx)
+	fmt.Printf("%#v\n", tx)
+	fmt.Println(ctID)
+	txHash, err := signBroadcast(txStr, acc, aeClient)
+	fmt.Println(txHash)
+	err = waitForTransaction(aeClient, txHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Confirm that contract was created")
+	time.Sleep(1000 * time.Millisecond)
+	_, err = aeClient.APIGetContractByID(ctID)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
