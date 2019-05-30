eb6cbd4b460f6d37fff0f741727ad3f3a8494b78
Andrew Chiw <randomshinichi4869@gmail.com>
2019-05-30 20:39:55 +0200

    refactor: setupNetwork(), setupAccounts(), usage of t.Log(), go-ish variable naming. Includes Spend Transaction Integration Test

--- integration_test/spend_test.go
+++ integration_test/spend_test.go
@@ -1,8 +1,8 @@
 package integrationtest
 
 import (
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/aeternity/aepp-sdk-go/aeternity"
 	"github.com/aeternity/aepp-sdk-go/utils"
@@ -11,67 +11,53 @@ import (
 // Tests for 2 things: sending an amount that is max uint64, and that the node accepts the minimum fee
 // that is calculated via tx.EstimateFee().
 func TestSpendTx(t *testing.T) {
-	senderAccount, err := aeternity.AccountFromHexString(senderPrivateKey)
-	if err != nil {
-		t.Fatal(err)
-	}
-	recipient := "ak_Egp9yVdpxmvAfQ7vsXGvpnyfNq71msbdUpkMNYGTeTe8kPL3v"
-	message := "Hello World"
-
-	aeternity.Config.Node.URL = nodeURL
-	aeternity.Config.Node.NetworkID = networkID
-	aeCli := aeternity.NewClient(aeternity.Config.Node.URL, false)
+	node := setupNetwork(t)
+	alice, bob := setupAccounts(t)
 
 	amount := utils.RequireBigIntFromString("18446744073709551615") // max uint64
 	fee := utils.NewBigIntFromUint64(uint64(2e13))
-	var expectedAmount = utils.NewBigInt()
+	msg := "Hello World"
 
 	// In case the recipient account already has funds, get recipient's account info. If it exists, expectedAmount = existing balance + amount + fee
-	recipientAccount, err := aeCli.APIGetAccount(recipient)
+	expected := utils.NewBigInt()
+	bobState, err := node.APIGetAccount(bob.Address)
 	if err != nil {
-		expectedAmount.Set(amount.Int)
+		expected.Set(amount.Int)
 	} else {
-		expectedAmount.Add(recipientAccount.Balance.Int, amount.Int)
+		expected.Add(bobState.Balance.Int, amount.Int)
 	}
 
-	ttl, nonce, err := aeCli.GetTTLNonce(sender, aeternity.Config.Client.TTL)
+	ttl, nonce, err := node.GetTTLNonce(sender, aeternity.Config.Client.TTL)
 	if err != nil {
 		t.Fatalf("Error in GetTTLNonce(): %v", err)
 	}
 
 	// create the SpendTransaction
-	tx := aeternity.NewSpendTx(sender, recipient, *amount, *fee, message, ttl, nonce)
+	tx := aeternity.NewSpendTx(sender, bob.Address, *amount, *fee, msg, ttl, nonce)
 	// minimize the fee to save money!
-	estimatedFee, _ := tx.FeeEstimate()
-	fmt.Println("Estimated vs Actual Fee:", estimatedFee, tx.Fee)
-	tx.Fee = *estimatedFee
+	est, _ := tx.FeeEstimate()
+	t.Log("Estimated vs Actual Fee:", est, tx.Fee)
+	tx.Fee = *est
 
-	base64TxMsg, err := aeternity.BaseEncodeTx(&tx)
+	txB64, err := aeternity.BaseEncodeTx(&tx)
 	if err != nil {
 		t.Fatalf("Base64 encoding errored out: %v", err)
 	}
-	fmt.Println(base64TxMsg)
+	t.Log(txB64)
 
 	// sign the transaction, output params for debugging
-	signedBase64TxMsg, _, _, err := aeternity.SignEncodeTxStr(senderAccount, base64TxMsg, aeternity.Config.Node.NetworkID)
+	_, err = signBroadcast(txB64, alice, node)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	fmt.Println(signedBase64TxMsg)
-
-	// send the signed transaction to the node
-	err = aeCli.BroadcastTransaction(signedBase64TxMsg)
-	if err != nil {
-		t.Fatalf("Error while broadcasting transaction: %v", err)
-	}
-
 	// check the recipient's balance
-	recipientAccount, err = aeCli.APIGetAccount(recipient)
+	time.Sleep(200 * time.Millisecond)
+	bobState, err = node.APIGetAccount(bob.Address)
 	if err != nil {
 		t.Fatalf("Couldn't get recipient's account data: %v", err)
 	}
 
-	if recipientAccount.Balance.Cmp(expectedAmount.Int) != 0 {
-		t.Fatalf("Recipient should have %v, but has %v instead", expectedAmount.String(), recipientAccount.Balance.String())
+	if bobState.Balance.Cmp(expected.Int) != 0 {
+		t.Fatalf("Recipient should have %v, but has %v instead", expected.String(), bobState.Balance.String())
 	}
 }
