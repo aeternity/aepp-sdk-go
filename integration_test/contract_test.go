b3dc468597ed1a5b01b083c9bd8f8bb2fa37fe5f
Andrew Chiw <randomshinichi4869@gmail.com>
2019-05-31 03:56:28 +0200

    refactor: integration tests cleaned up with closures and test setup functions. More robust because it waits longer in delayableCode

--- integration_test/contract_test.go
+++ integration_test/contract_test.go
@@ -3,21 +3,15 @@ package integrationtest
 import (
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/aeternity/aepp-sdk-go/aeternity"
 	"github.com/aeternity/aepp-sdk-go/utils"
 )
 
 func TestContracts(t *testing.T) {
-	acc, err := aeternity.AccountFromHexString(senderPrivateKey)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	aeternity.Config.Node.NetworkID = networkID
-	aeClient := aeternity.NewClient(nodeURL, false)
-	contractsAlice := aeternity.Contract{Client: aeClient, Account: acc}
+	alice, _ := setupAccounts(t)
+	aeClient := setupNetwork(t)
+	contractsAlice := aeternity.Contract{Client: aeClient, Account: alice}
 
 	var ctID string
 	var callData string
@@ -25,47 +19,37 @@ func TestContracts(t *testing.T) {
 
 	code := "cb_+QP1RgKgpVq1Ib2r2ug+UktHvfWSQ8P35HJQHM6qikqBu1DwgtT5Avv5ASqgaPJnYzj/UIg5q6R3Se/6i+h+8oTyB/s9mZhwHNU4h8WEbWFpbrjAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKD//////////////////////////////////////////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAuEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+QHLoLnJVvKLMUmp9Zh6pQXz2hsiCcxXOSNABiu2wb2fn5nqhGluaXS4YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP//////////////////////////////////////////7kBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEA//////////////////////////////////////////8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA///////////////////////////////////////////uMxiAABkYgAAhJGAgIBRf7nJVvKLMUmp9Zh6pQXz2hsiCcxXOSNABiu2wb2fn5nqFGIAAMBXUIBRf2jyZ2M4/1CIOaukd0nv+ovofvKE8gf7PZmYcBzVOIfFFGIAAK9XUGABGVEAW2AAGVlgIAGQgVJgIJADYAOBUpBZYABRWVJgAFJgAPNbYACAUmAA81tZWWAgAZCBUmAgkANgABlZYCABkIFSYCCQA2ADgVKBUpBWW2AgAVFRWVCAkVBQgJBQkFZbUFCCkVBQYgAAjFaFMi4xLjBJtQib"
 	callData = "cb_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACC5yVbyizFJqfWYeqUF89obIgnMVzkjQAYrtsG9n5+Z6gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAnHQYrA=="
-	fmt.Println("Contract Create")
-	tx, err := contractsAlice.ContractCreateTx(code, callData, aeternity.Config.Client.Contracts.VMVersion, aeternity.Config.Client.Contracts.ABIVersion, aeternity.Config.Client.Contracts.Deposit, aeternity.Config.Client.Contracts.Amount, *utils.NewBigIntFromUint64(1e5), aeternity.Config.Client.Contracts.GasPrice, *utils.NewBigIntFromUint64(564480000000000))
+	create, err := contractsAlice.ContractCreateTx(code, callData, aeternity.Config.Client.Contracts.VMVersion, aeternity.Config.Client.Contracts.ABIVersion, aeternity.Config.Client.Contracts.Deposit, aeternity.Config.Client.Contracts.Amount, *utils.NewBigIntFromUint64(1e5), aeternity.Config.Client.Contracts.GasPrice, *utils.NewBigIntFromUint64(564480000000000))
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	ctID, _ = tx.ContractID()
-	txStr, _ := aeternity.BaseEncodeTx(&tx)
-	fmt.Printf("%#v\n", tx)
-	fmt.Println(ctID)
-	txHash, err = signBroadcast(txStr, acc, aeClient)
-	fmt.Println(txHash)
+	ctID, _ = create.ContractID()
+	fmt.Printf("Create %s, %#v\n", ctID, create)
+	txHash = signBroadcast(t, &create, alice, aeClient)
 	err = waitForTransaction(aeClient, txHash)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	fmt.Println("Confirm that contract was created")
-	time.Sleep(1000 * time.Millisecond)
-	_, err = aeClient.APIGetContractByID(ctID)
-	if err != nil {
-		t.Fatal(err)
+	// Confirm that contract was created
+	getContract := func() {
+		_, err = aeClient.APIGetContractByID(ctID)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
+	delay(getContract)
 
-	fmt.Println("Contract Call")
 	callData = "cb_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACBo8mdjOP9QiDmrpHdJ7/qL6H7yhPIH+z2ZmHAc1TiHxQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACo7dbVl"
 	callTx, err := contractsAlice.ContractCallTx(ctID, callData, aeternity.Config.Client.Contracts.VMVersion, aeternity.Config.Client.Contracts.ABIVersion, aeternity.Config.Client.Contracts.Amount, *utils.NewBigIntFromUint64(1e5), aeternity.Config.Client.Contracts.GasPrice, *utils.NewBigIntFromUint64(665480000000000))
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	fmt.Printf("%#v\n", callTx)
-	fmt.Println(callTx.FeeEstimate())
-	callTxStr, _ := aeternity.BaseEncodeTx(&callTx)
-	fmt.Println(callTxStr)
-	txHash, err = signBroadcast(callTxStr, acc, aeClient)
-	fmt.Println(txHash)
+	fmt.Printf("Call %#v\n", callTx)
+	txHash = signBroadcast(t, &callTx, alice, aeClient)
 
 	err = waitForTransaction(aeClient, txHash)
 	if err != nil {
 		t.Fatal(err)
 	}
-
 }
--- integration_test/spend_test.go
+++ integration_test/spend_test.go
@@ -1,8 +1,8 @@
 package integrationtest
 
 import (
+	"fmt"
 	"testing"
-	"time"
 
 	"github.com/aeternity/aepp-sdk-go/aeternity"
 	"github.com/aeternity/aepp-sdk-go/utils"
@@ -36,28 +36,22 @@ func TestSpendTx(t *testing.T) {
 	tx := aeternity.NewSpendTx(sender, bob.Address, *amount, *fee, msg, ttl, nonce)
 	// minimize the fee to save money!
 	est, _ := tx.FeeEstimate()
-	t.Log("Estimated vs Actual Fee:", est, tx.Fee)
+	fmt.Println("Estimated vs Actual Fee:", est, tx.Fee)
 	tx.Fee = *est
 
-	txB64, err := aeternity.BaseEncodeTx(&tx)
-	if err != nil {
-		t.Fatalf("Base64 encoding errored out: %v", err)
-	}
-	t.Log(txB64)
-
 	// sign the transaction, output params for debugging
-	_, err = signBroadcast(txB64, alice, node)
-	if err != nil {
-		t.Fatal(err)
-	}
+	_ = signBroadcast(t, &tx, alice, node)
 	// check the recipient's balance
-	time.Sleep(200 * time.Millisecond)
-	bobState, err = node.APIGetAccount(bob.Address)
-	if err != nil {
-		t.Fatalf("Couldn't get recipient's account data: %v", err)
+
+	getBobsAccount := func() {
+		bobState, err = node.APIGetAccount(bob.Address)
+		if err != nil {
+			t.Fatalf("Couldn't get Bob's account data: %v", err)
+		}
 	}
+	delay(getBobsAccount)
 
 	if bobState.Balance.Cmp(expected.Int) != 0 {
-		t.Fatalf("Recipient should have %v, but has %v instead", expected.String(), bobState.Balance.String())
+		t.Fatalf("Bob should have %v, but has %v instead", expected.String(), bobState.Balance.String())
 	}
 }
