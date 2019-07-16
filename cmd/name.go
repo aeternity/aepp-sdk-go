b1a5431a2caaf452d75c8dea052365ecf3223390
Andrew Chiw <randomshinichi4869@gmail.com>
2019-07-16 12:19:47 +0200

    feature: CLI AENS lookup

---
+++ cmd/name.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aeternity/aepp-sdk-go/aeternity"
+
+	"github.com/spf13/cobra"
+)
+
+var nameCmd = &cobra.Command{
+	Use:   "name",
+	Short: "Lookup a name on AENS",
+	Long:  ``,
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		node := newAeNode()
+		err := nameFunc(node, args[0])
+		return err
+	},
+}
+
+func nameFunc(conn aeternity.GetNameEntryByNamer, name string) (err error) {
+	ans, err := conn.GetNameEntryByName(name)
+	if err != nil {
+		return err
+	}
+
+	o, err := json.MarshalIndent(ans, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(o))
+	return nil
+}
+func init() {
+	RootCmd.AddCommand(nameCmd)
+}
