08f6d6cbd8b0bb446b39c67062bb2c607c8ed449
Andrew Chiw <randomshinichi4869@gmail.com>
2019-07-25 16:34:50 +0200

    refactor: tx spend/contract subcommand unittests should rely on HelperInterface, not API Interfaces. Also, fixed tx spend issue where it was printing what you wanted, instead of the actual tx's parameters

--- cmd/tx_test.go
+++ cmd/tx_test.go
@@ -12,8 +12,8 @@ var bob = "ak_Egp9yVdpxmvAfQ7vsXGvpnyfNq71msbdUpkMNYGTeTe8kPL3v"
 
 func Test_txSpendFunc(t *testing.T) {
 	type args struct {
-		conn aeternity.GetAccounter
-		args []string
+		helpers aeternity.HelpersInterface
+		args    []string
 	}
 	tests := []struct {
 		name    string
@@ -23,15 +23,15 @@ func Test_txSpendFunc(t *testing.T) {
 		{
 			name: "Alice sends 10 to Bob",
 			args: args{
-				conn: &mockGetAccounter{account: `{"balance":1600000000000000077131306000000000000000,"id":"ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi","kind":"basic","nonce":0}`},
-				args: []string{alice, bob, "10"},
+				helpers: &mockHelpers{},
+				args:    []string{alice, bob, "10"},
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := txSpendFunc(tt.args.conn, tt.args.args); (err != nil) != tt.wantErr {
+			if err := txSpendFunc(tt.args.helpers, tt.args.args); (err != nil) != tt.wantErr {
 				t.Errorf("txSpendFunc() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -62,12 +62,9 @@ func TestTxDumpRaw(t *testing.T) {
 }
 
 func Test_txContractCreateFunc(t *testing.T) {
-	aeternity.GetTTLNonce = func(c aeternity.GetHeightAccounter, accountID string, offset uint64) (height uint64, nonce uint64, err error) {
-		return 2, 1337, nil
-	}
 	type args struct {
-		conn getHeightAccounter
-		args []string
+		helpers aeternity.HelpersInterface
+		args    []string
 	}
 	tests := []struct {
 		name    string
@@ -77,15 +74,15 @@ func Test_txContractCreateFunc(t *testing.T) {
 		{
 			name: "Deploy SimpleStorage with alice (unsigned)",
 			args: args{
-				conn: &mockgetHeightAccounter{},
-				args: []string{alice, contractSimpleStorageBytecode, contractSimpleStorageInitCalldata},
+				helpers: &mockHelpers{},
+				args:    []string{alice, contractSimpleStorageBytecode, contractSimpleStorageInitCalldata},
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := txContractCreateFunc(tt.args.conn, tt.args.args); (err != nil) != tt.wantErr {
+			if err := txContractCreateFunc(tt.args.helpers, tt.args.args); (err != nil) != tt.wantErr {
 				t.Errorf("txContractCreateFunc() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
