d7bf5fd3cc36f155ab3e06eee94ffce64bd1158a
Andrew Chiw <randomshinichi4869@gmail.com>
2019-10-16 17:13:26 +0200

    feature: higher level AENS supports NameFee

--- aeternity/aens.go
+++ aeternity/aens.go
@@ -1,6 +1,8 @@
 package aeternity
 
 import (
+	"math/big"
+
 	"github.com/aeternity/aepp-sdk-go/v6/account"
 	"github.com/aeternity/aepp-sdk-go/v6/config"
 	"github.com/aeternity/aepp-sdk-go/v6/naet"
@@ -9,7 +11,7 @@ import (
 
 // RegisterName allows one to easily register a name on AENS. It does the
 // preclaim, transaction sending, confirmation and claim for you.
-func RegisterName(n naet.NodeInterface, acc *account.Account, name string) (signedTxStr, hash, signature string, blockHeight uint64, blockHash string, err error) {
+func RegisterName(n naet.NodeInterface, acc *account.Account, name string, nameFee *big.Int) (signedTxStr, hash, signature string, blockHeight uint64, blockHash string, err error) {
 	status, err := n.GetStatus()
 	if err != nil {
 		return
@@ -33,7 +35,7 @@ func RegisterName(n naet.NodeInterface, acc *account.Account, name string) (sign
 	if err != nil {
 		return
 	}
-	claimTx := transactions.NewNameClaimTx(acc.Address, name, nameSalt, config.Client.Fee, ttl, nonce)
+	claimTx := transactions.NewNameClaimTx(acc.Address, name, nameSalt, nameFee, config.Client.Fee, ttl, nonce)
 	transactions.CalculateFee(claimTx)
 	signedTxStr, hash, signature, blockHeight, blockHash, err = SignBroadcastWaitTransaction(claimTx, acc, n.(*naet.Node), networkID, config.Client.WaitBlocks)
 	if err != nil {
