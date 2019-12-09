a11c714db9b46d64f33f1ce4dd9a39c459243aa8
Andrew Chiw <randomshinichi4869@gmail.com>
2019-12-10 00:06:28 +0100

    refactor: AENS higher level TestRegisterName uses Broadcaster; CreateContract returns ctID; CallContract introduced

--- integration_test/aens_higherlevel_test.go
+++ integration_test/aens_higherlevel_test.go
@@ -11,9 +11,13 @@ func TestRegisterName(t *testing.T) {
 	n := setupNetwork(t, privatenetURL, false)
 	alice, _ := setupAccounts(t)
 
+	broadcaster, err := aeternity.NewBroadcaster(alice, n)
+	if err != nil {
+		t.Fatal(err)
+	}
 	name := "somelongnamefdsafdffsa.chain"
 	nameFee := transactions.CalculateMinNameFee(name)
-	_, _, _, _, _, err := aeternity.RegisterName(n, alice, name, nameFee)
+	_, _, _, _, _, err = aeternity.RegisterName(n, broadcaster, name, nameFee)
 	if err != nil {
 		t.Error(err)
 	}
--- integration_test/contract_higherlevel_test.go
+++ integration_test/contract_higherlevel_test.go
@@ -20,7 +20,30 @@ contract SimpleStorage =
   function get() : int = state.data
   stateful function set(value : int) = put(state{data = value})`
 
-	_, _, _, _, _, err := aeternity.CreateContract(n, c, alice, simplestorage, "init", []string{"42"}, config.CompilerBackendFATE)
+	_, _, _, _, _, _, err := aeternity.CreateContract(n, c, alice, simplestorage, "init", []string{"42"}, config.CompilerBackendFATE)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCallContract(t *testing.T) {
+	n := setupNetwork(t, privatenetURL, false)
+	c := naet.NewCompiler("http://localhost:3080", false)
+	alice, _ := setupAccounts(t)
+
+	simplestorage := `
+contract SimpleStorage =
+  record state = { data : int }
+  entrypoint init(value : int) : state = { data = value }
+  function get() : int = state.data
+  stateful function set(value : int) = put(state{data = value})`
+
+	ctID, _, _, _, _, _, err := aeternity.CreateContract(n, c, alice, simplestorage, "init", []string{"42"}, config.CompilerBackendFATE)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, _, _, _, err = aeternity.CallContract(n, c, alice, ctID, simplestorage, "get", []string{}, config.CompilerBackendFATE)
 	if err != nil {
 		t.Error(err)
 	}
