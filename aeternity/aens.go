dd2b41b6943a54e91fec9ba049dc3c09d244ff6c
Andrew Chiw <randomshinichi4869@gmail.com>
2019-12-16 11:40:30 +0100

    refactor: AENS uses Context

--- aeternity/aens.go
+++ aeternity/aens.go
@@ -4,34 +4,35 @@ import (
 	"math/big"
 
 	"github.com/aeternity/aepp-sdk-go/v7/config"
-	"github.com/aeternity/aepp-sdk-go/v7/naet"
 	"github.com/aeternity/aepp-sdk-go/v7/transactions"
 )
 
-type nodeStatusHeightAccounterBroadcaster interface {
-	naet.GetStatuser
-	naet.GetAccounter
-	broadcastWaitTransactionNodeCapabilities
+type AENS struct {
+	ctx ContextInterface
+}
+
+func NewAENS(ctx ContextInterface) *AENS {
+	return &AENS{ctx: ctx}
 }
 
 // RegisterName allows one to easily register a name on AENS. It does the
 // preclaim, transaction sending, confirmation and claim for you.
-func (ctx *Context) RegisterName(name string, nameFee *big.Int) (claimTxReceipt *TxReceipt, err error) {
-	preclaimTx, nameSalt, err := transactions.NewNamePreclaimTx(ctx.Account.Address, name, ctx.TTLNoncer)
+func (aens *AENS) RegisterName(name string, nameFee *big.Int) (claimTxReceipt *TxReceipt, err error) {
+	preclaimTx, nameSalt, err := transactions.NewNamePreclaimTx(aens.ctx.SenderAccount(), name, aens.ctx.TTLNoncer())
 	if err != nil {
 		return
 	}
-	_, err = ctx.SignBroadcastWait(preclaimTx, config.Client.WaitBlocks)
-	if err != nil {
-		return
-	}
-
-	claimTx, err := transactions.NewNameClaimTx(ctx.Account.Address, name, nameSalt, nameFee, ctx.TTLNoncer)
+	_, err = aens.ctx.SignBroadcastWait(preclaimTx, config.Client.WaitBlocks)
 	if err != nil {
 		return
 	}
 
-	claimTxReceipt, err = ctx.SignBroadcastWait(claimTx, config.Client.WaitBlocks)
+	claimTx, err := transactions.NewNameClaimTx(aens.ctx.SenderAccount(), name, nameSalt, nameFee, aens.ctx.TTLNoncer())
+	if err != nil {
+		return
+	}
+
+	claimTxReceipt, err = aens.ctx.SignBroadcastWait(claimTx, config.Client.WaitBlocks)
 	if err != nil {
 		return
 	}
